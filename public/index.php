178e4dde1afdc640fc6b01e27633ac15c905e804
Марат <maratkazakbiev@gmail.com>
2024-05-21 23:11:29 +0400

    upd

--- public/index.php
+++ public/index.php
@@ -1,10 +1,40 @@
+#!/usr/bin/php
 <?php
 declare(strict_types =1 );
 use Maratkazakbiev\HtOtus1\MathBracketsResolver;
 require __DIR__ . '/../vendor/autoload.php';
 
-$path = readline("Укажите путь к файлу: ");
-$exercise = file_get_contents($path);
+$short_options = '';
+$short_options .= 'p:';
+$long_options = array('port:');
+$port = getopt($short_options, $long_options)['port'];
 
-$new = new MathBracketsResolver($exercise);
-$new->Resolve();
+
+$socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+socket_bind($socket, '127.0.0.1', (int)$port);
+socket_listen($socket, 2);
+
+do {
+    $msgsock = socket_accept($socket);
+
+    $msg = "----Напишите свой пример----";
+
+    socket_write($msgsock, $msg);
+
+
+    do{
+        $buffer = socket_read($msgsock, 2048, PHP_BINARY_READ );
+        $buffer = trim($buffer);
+
+        if ($buffer == 'Выход'){
+            break;
+        }
+
+        $new = new MathBracketsResolver($buffer);
+        $answer = $new->Resolve();
+
+        socket_write($msgsock, $answer);
+    }while(true);
+    socket_close($msgsock);
+}while(true);
+socket_close($socket);
