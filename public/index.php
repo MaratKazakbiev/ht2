b1120d240ae219464928d279688d972eb7a94119
Марат <maratkazakbiev@gmail.com>
2024-05-22 22:28:59 +0400

    add gitignore, добавлены потоки

--- public/index.php
+++ public/index.php
@@ -9,32 +9,41 @@ $short_options .= 'p:';
 $long_options = array('port:');
 $port = getopt($short_options, $long_options)['port'];
 
-
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_bind($socket, '127.0.0.1', (int)$port);
 socket_listen($socket, 2);
 
-do {
-    $msgsock = socket_accept($socket);
+$pid = pcntl_fork();
 
-    $msg = "----Напишите свой пример----";
+if($pid == -1){
+    return 'Не получилось создать поток';
+}elseif ($pid){
+//    Тут лежит родительский процесс
+    pcntl_wait($code);
+}else{
+    do {
+        $msgsock = socket_accept($socket);
 
-    socket_write($msgsock, $msg);
+        $msg = "----Напишите свой пример----";
+
+        socket_write($msgsock, $msg);
 
 
-    do{
-        $buffer = socket_read($msgsock, 2048, PHP_BINARY_READ );
-        $buffer = trim($buffer);
+        do{
+            $buffer = socket_read($msgsock, 2048, PHP_BINARY_READ );
+            $buffer = trim($buffer);
 
-        if ($buffer == 'Выход'){
-            break;
-        }
+            if ($buffer == 'Выход'){
+                break;
+            }
 
-        $new = new MathBracketsResolver($buffer);
-        $answer = $new->Resolve();
+            $new = new MathBracketsResolver($buffer);
+            $answer = $new->Resolve();
 
-        socket_write($msgsock, $answer);
+            socket_write($msgsock, $answer);
+        }while(true);
+        socket_close($msgsock);
     }while(true);
-    socket_close($msgsock);
-}while(true);
-socket_close($socket);
+    socket_close($socket);
+    exit(123);
+}
