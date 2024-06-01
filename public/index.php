9dced87e041be9804639ee318fe407501867102e
Марат <maratkazakbiev@gmail.com>
2024-06-01 20:03:28 +0400

    new logic

--- public/index.php
+++ public/index.php
@@ -2,19 +2,27 @@
 <?php
 declare(strict_types=1);
 use Maratkazakbiev\HtOtus1\MathBracketsResolver;
+use Symfony\Component\Yaml\Yaml;
+
 require __DIR__ . '/../vendor/autoload.php';
 
-$short_options = '';
-$short_options .= 'p:';
-$long_options = array('port:');
-$port = getopt($short_options, $long_options)['port'];
+// Читаем конфигурационный файл
+$config = Yaml::parse(file_get_contents('config.yaml'));
+$port = $config['port'];
 
 $socket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
 socket_bind($socket, '127.0.0.1', (int)$port);
 socket_listen($socket, 2);
 
+// Устанавливаем слушатель сигнала SIGHUP
+pcntl_signal(SIGHUP, 'reloadConfig');
+
+// Хранилище для принятых подключений
+$connections = [];
+
 while (true) {
     $msgsock = socket_accept($socket);
+    $connections[] = $msgsock; // Сохраняем принятое подключение
 
     $msg = "----Напишите свой пример----" . PHP_EOL;
     socket_write($msgsock, $msg);
@@ -36,9 +44,34 @@ while (true) {
         } while (true);
         socket_close($msgsock);
         exit(); // Завершаем дочерний процесс
-    } else { // Родительский процесс
-        // Продолжаем ожидать новых подключений
     }
 }
 
-socket_close($socket);
+// Функция-обработчик сигнала SIGHUP
+function reloadConfig($signal) {
+    global $socket, $connections, $port;
+
+    // Закрываем старый сокет
+    socket_close($socket);
+
+    // Перечитываем конфигурационный файл
+    $config = Yaml::parse(file_get_contents('config.yaml'));
+    $newPort = $config['port'];
+
+    // Создаем новый сокет
+    $newSocket = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+    socket_bind($newSocket, '127.0.0.1', (int)$newPort);
+    socket_listen($newSocket, 2);
+
+    // Перенаправляем все принятые подключения на новый порт
+    foreach ($connections as $connection) {
+        socket_close($connection);
+        $newConnection = socket_create(AF_INET, SOCK_STREAM, SOL_TCP);
+        socket_connect($newConnection, '127.0.0.1', (int)$newPort);
+        $connections[] = $newConnection;
+    }
+
+    // Обновляем глобальные переменные
+    $socket = $newSocket;
+    $port = $newPort;
+}
